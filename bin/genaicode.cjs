721b9f85e52236b5d18e19ca1bb2242d9d3eaffb
Grzegorz Tańczyk <fb@tanczyk.pl>
2024-08-29 22:21:01 +0200

    node >=20 for genaicode-dev.js

--- bin/genaicode.cjs
+++ bin/genaicode.cjs
@@ -9,6 +9,13 @@ const devMode = require('fs').existsSync(`${__dirname}/../src`);
 const forceDist = process.argv.indexOf('--force-dist') >= 0;
 
 if (devMode && !forceDist) {
+  pleaseUpgradeNode({
+    name: "genaicode-dev.js",
+    engines: {
+      node: ">=20",
+    }, 
+  });
+
   import('./genaicode-dev.js');
 } else {
   import('../dist/main/codegen.js').then(({ runCodegen }) => runCodegen());
