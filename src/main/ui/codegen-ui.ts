06e7a0d29a709edb6a87848dd3432d71fc1d2e00
Grzegorz Tańczyk <fb@tanczyk.pl>
2024-09-15 02:32:21 +0200

    ui action handler

--- src/main/ui/codegen-ui.ts
+++ src/main/ui/codegen-ui.ts
@@ -1,10 +1,16 @@
 import { CodegenOptions } from '../codegen-types.js';
 import { startServer } from './backend/server.js';
+import { Service } from './backend/service.js';
+import { registerUserActionHandlers } from './user-action-handlers.js';
 
 export async function runCodegenUI(options: CodegenOptions) {
   console.log('Starting Genaicode Web UI');
 
-  await startServer(options);
+  const service = new Service(options);
+
+  registerUserActionHandlers(service);
+
+  await startServer(service);
 
   console.log('Genaicode Web UI started');
 }
---
+++ src/main/ui/user-action-handlers.ts
@@ -0,0 +1,9 @@
+import { registerConfirmHandler, registerInputHandler } from '../common/user-actions.js';
+import { Service } from './backend/service.js';
+
+export function registerUserActionHandlers(service: Service) {
+  registerInputHandler((message) => service.askQuestion(message));
+  registerConfirmHandler(
+    async (message: string /*, defaultValue: boolean*/) => (await service.askQuestion(message)) === 'yes',
+  );
+}
